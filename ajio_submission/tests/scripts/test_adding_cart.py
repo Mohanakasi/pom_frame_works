c248cb21977a3ae058bb46c0c810d5101a0fa484
Mohanakasi <mohanakasi.s3@gmail.com>
2023-11-08 14:51:00 +0530

    updating ajio project

--- ajio_submission/tests/scripts/test_adding_cart.py
+++ ajio_submission/tests/scripts/test_adding_cart.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.by import By
 from ajio_submission.page_home.pages.home_page import menu_header_validation
 from ajio_submission.page_home.pages.items_list_page import home_items_list_page
 from time import sleep
-
+from pytest import mark
 @pytest.mark.usefixtures('setup')
 class Test_cart_validations:
-
+    @mark.home_item_check
     def test_cart_item_check_item_and_home_page(self, setup):
         cart_obj1 = home_items_list_page(setup)
         cart_obj1.select_item_from_list()
@@ -16,13 +16,14 @@ class Test_cart_validations:
         cart_obj1.switching_to_home_page()
         cart_obj1.checking_cart_in_home_page()
 
-
+    @mark.item_delete
     def test_adding_cart_delete(self, setup):
         cart_obj2 = home_items_list_page(setup)
         cart_obj2.select_item_from_list()
         cart_obj2.clicking_item_in_sungasses()
         cart_obj2.deleting_an_item_shipping_page(cart_obj2.mouse_hover_to_sunglass_and_click_proceed_to_bag())
 
+    @mark.log_check
     def test_ajio_logo_test(self, setup):
         cart_obj3 = home_items_list_page(setup)
         cart_obj3.select_item_from_list()
